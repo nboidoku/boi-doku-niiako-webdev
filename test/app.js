237bce29ea9443a0a516d816192a05495a21a3a8
nboidoku <niiakoboi-doku@Niis-MacBook-Pro.local>
2017-05-10 13:45:48 -0400

    test changed

--- test/app.js
+++ test/app.js
@@ -4,15 +4,24 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+        console.log('here')
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += '@ds161487.mlab.com:61487/heroku_qqmcl53h'; // user yours
+    }
+
+    /*if(process.env.MLAB_USERNAME) {
+        console.log('here')
         connectionString = process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
             process.env.MLAB_HOST + ':' +
             process.env.MLAB_PORT + '/' +
             process.env.MLAB_APP_NAME;
-    }
+    }*/
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
