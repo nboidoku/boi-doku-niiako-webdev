c2fb88c4683f5e0df046efff34589d9f2207c421
nboidoku <niiakoboi-doku@Niis-MacBook-Pro.local>
2017-06-12 14:44:34 -0400

    more updates

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var app = require('./express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//app.use(cookieParser());
+//app.use(session({secret: process.env.SESSION_SECRET}));
+//app.use(session({secret: "put some text here"}));
 
 app.set('view engine', 'ejs');
 
